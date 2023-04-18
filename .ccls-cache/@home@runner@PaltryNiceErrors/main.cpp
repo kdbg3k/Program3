78d89a556082d27b96303ffc1691d7a118caa0fd
KathrynBohanan <16028128-KathrynBohanan@users.noreply.replit.com>
2023-04-18 04:57:28 +0000

    first
    first commit

--- .ccls-cache/@home@runner@PaltryNiceErrors/main.cpp
+++ .ccls-cache/@home@runner@PaltryNiceErrors/main.cpp
@@ -13,20 +13,20 @@ int main() {
   std::cout << "\nPostfix expression: " << postfix << std::endl;
   
 
-Queue<int> Q;
-Q.enqueue(1);
+Queue<int>* Q;
+Q->enqueue(1);
 Q.enqueue(2);
 Q.enqueue(3);
 Q.enqueue(4);
 Q.enqueue(5);
 
-std::cout << "Front element: " << Q.peek() << std::endl;
+std::cout << "Front element: " << Q->peek() << std::endl;
 
-std::cout << "Dequeue: " << Q.dequeue() << std::endl;
-std::cout << "Dequeue: " << Q.dequeue() << std::endl;
+std::cout << "Dequeue: " << Q->dequeue() << std::endl;
+std::cout << "Dequeue: " << Q->dequeue() << std::endl;
 
-std::cout << "Is queue empty? " << Q.is_empty() << std::endl;
+std::cout << "Is queue empty? " << Q->is_empty() << std::endl;
 
-std::cout << "Total elements in queue: " << Q.get_size() << std::endl;
+std::cout << "Total elements in queue: " << Q->get_size() << std::endl;
   
 }
